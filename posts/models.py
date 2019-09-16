a63358754ab6ded6fb7983a4fe9c91d9b72a16ea
Laura <lcrossle@gmu.edu>
2019-09-16 13:42:08 -0400

    initial commit

--- posts/models.py
+++ posts/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Post(models.Model):
     text = models.TextField()
 
-    def _str_(self):
+    def __str__(self):
         return self.text[:50]
 
 # Create your models here.
